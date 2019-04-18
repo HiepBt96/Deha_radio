fd4f20d8e5b0a7c0f0b3bf235c53cb4c3fec0eea
Bùi Thế Hiệp <buithehiep.it@gmail.com>
2019-04-18 15:42:32 +0700

    Initial Commit

---
+++ Deha_RadioTests/Deha_RadioTests.swift
@@ -0,0 +1,34 @@
+//
+//  Deha_RadioTests.swift
+//  Deha_RadioTests
+//
+//  Created by Bui The Hiep on 4/18/19.
+//  Copyright © 2019 BuiTheHiep. All rights reserved.
+//
+
+import XCTest
+@testable import Deha_Radio
+
+class Deha_RadioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
